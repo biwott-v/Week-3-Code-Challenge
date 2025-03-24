5796da7f2fd979599a4d3677a2b299bc9a823509
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-24 09:12:18 +0300

    improves functionality

--- src/index.js
+++ src/index.js
@@ -3,26 +3,63 @@ document.addEventListener("DOMContentLoaded",(event)=>{
     let movieImage=document.querySelector("#image");
     movieImage.src=data.films[x].poster;
     let movieTitle=document.querySelector("#title");
-    movieTitle.textcontent=data.films[x].title 
+    movieTitle.textContent=data.films[x].title 
     let movieRuntime=document.querySelector("#minruntime");
     movieRuntime.textContent=data.films[x].runtime;
+    movieRuntime.style.color="blue";
     let movieDescription =document.querySelector("#description");
     movieDescription.textContent=data.films[x].description;
     let movieTime = document.querySelector("#time");
-    movieTime.textContent = `${data.films[x].showtime} : ${data.films[x].ticketsold}`;
+    movieTickets =document.querySelector("#tickets");
+    movieTime.textContent = `${data.films[x].showtime} : `;
+    movieTickets.textContent=`${data.films[x].capacity-data.films[x].tickets_sold} tickets remaining`;
+    tickets=data.films[x].capacity-data.films[x].tickets_sold;
+    movieTickets.style.display="inline"
+    movieTime.style.display="inline";
+    let landTitle = document.querySelector("#land");
+    landTitle.remove();
   }
-  movieList=document.querySelector("#movielist");
+  let tickets;
+  let button=document.querySelector("#btn");
+  let movieList=document.querySelector("#films");
   fetch("db.json")
   .then(req=>req.json())
   .then((data)=>{
-    for(let i=0; i<data.length;i++){
-      let newMovie=createElement("li");
+    console.log(data.length);
+    console.log(data)
+    for(let i=0; i<15;i++){
+      let breakLine=document.createElement("br")
+      let horizontalRule=document.createElement("hr")
+      let newMovie=document.createElement("li");
       newMovie.textContent=data.films[i].title;
-      newMovie.setAttribute("id",i);
       movieList.appendChild(newMovie);
-      newMovie.addEventListener("click",change(i,data));
+      newbtn=document.createElement("button");
+      newMovie.appendChild(newbtn);
+      newbtn.textContent="DELETE";
+      newbtn.style.margin="3px";
+      newbtn.style.display="inline";
+      newbtn.addEventListener("click",(event)=>{
+        newMovie.remove();
+        horizontalRule.remove();
+        breakLine.remove();
+      })
+      movieList.appendChild(horizontalRule);  
+      movieList.appendChild(breakLine);
+      //newMovie.addEventListener("click",change(i++,data));
     }
+    change(0,data);
+    button.addEventListener("click",()=>{
+      if(tickets>0){
+        value=tickets--;
+       let movieTickets = document.querySelector("#tickets");
+       movieTickets.textContent=`${tickets} tickets remaining`;
+      }
+      else{
+        button.textContent="Sold Out";
+      }
+    })
   }) 
 
 })
+
 
