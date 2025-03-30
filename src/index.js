a3939c416c210b887008569903c668d702162625
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-30 23:33:40 +0300

    completes assignment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded",(event)=>{
     let movieTitle=document.querySelector("#title");
     movieTitle.textContent=data.films[x].title 
     let movieRuntime=document.querySelector("#minruntime");
-    movieRuntime.textContent=data.films[x].runtime;
+    movieRuntime.textContent=`${data.films[x].runtime} minutes`;
     movieRuntime.style.color="blue";
     let movieDescription =document.querySelector("#description");
     movieDescription.textContent=data.films[x].description;
@@ -14,6 +14,8 @@ document.addEventListener("DOMContentLoaded",(event)=>{
     movieTime.textContent = `${data.films[x].showtime} : `;
     movieTickets.textContent=`${data.films[x].capacity-data.films[x].tickets_sold} tickets remaining`;
     tickets=data.films[x].capacity-data.films[x].tickets_sold;
+
+    movieTime.style.backgroundColor="wheat"
     movieTickets.style.display="inline"
     movieTime.style.display="inline";
     let landTitle = document.querySelector("#land");
@@ -25,10 +27,8 @@ document.addEventListener("DOMContentLoaded",(event)=>{
   fetch("db.json")
   .then(req=>req.json())
   .then((data)=>{
-    console.log(data.length);
-    console.log(data)
+
     for(let i=0; i<15;i++){
-      let breakLine=document.createElement("br")
       let horizontalRule=document.createElement("hr")
       let newMovie=document.createElement("li");
       newMovie.textContent=data.films[i].title;
@@ -41,25 +41,36 @@ document.addEventListener("DOMContentLoaded",(event)=>{
       newbtn.addEventListener("click",(event)=>{
         newMovie.remove();
         horizontalRule.remove();
-        breakLine.remove();
+        fetch(`db.json/${data.films[i].id}`,{
+          method:"DELETE"
+
+        })
       })
+
       movieList.appendChild(horizontalRule);  
-      movieList.appendChild(breakLine);
-      //newMovie.addEventListener("click",change(i++,data));
     }
-    change(0,data);
+
+    change(1,data);
     button.addEventListener("click",()=>{
       if(tickets>0){
         value=tickets--;
-       let movieTickets = document.querySelector("#tickets");
-       movieTickets.textContent=`${tickets} tickets remaining`;
+        let movieTickets = document.querySelector("#tickets");
+        movieTickets.textContent=`${tickets} tickets remaining`;
+        fetch(`db.json/${data.films[1].id}`,{
+          method:"PATCH",
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify({
+            tickets_sold:data.films[1].tickets_sold + 1
+          })
+
+        })
       }
       else{
         button.textContent="Sold Out";
       }
-    })
+    })   
+
   }) 
-
 })
-
-
